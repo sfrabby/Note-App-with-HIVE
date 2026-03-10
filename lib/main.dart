0b2d99798665a9b306f13cef0092fae281229e66
sfrabby <fazlerabby6732@gmail.com>
2026-03-10 23:56:12 +0600

    Hive Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'View/HomePage/UI.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await Hive.initFlutter();
+   var myBox = await Hive.openBox("Note");
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
