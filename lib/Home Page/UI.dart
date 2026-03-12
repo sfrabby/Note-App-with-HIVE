de317f71edd1d21ffdb842fa40cf760882c28ea2
sfrabby <fazlerabby6732@gmail.com>
2026-03-12 23:43:14 +0600

    UI fully completed

--- lib/Home Page/UI.dart
+++ lib/Home Page/UI.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,16 +8,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Note App", style: TextStyle(color: Colors.white),),
+        title: Text("Note App", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.teal,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
       body: ListView.builder(
-
         itemCount: 10,
         itemBuilder: (context, index) {
-        return Card(child: ListTile(),);
-      },),
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(child: Text("${index + 1}")),
+              title: Text("This Is Title"),
+              subtitle: Text("data"),
+              trailing: PopupMenuButton(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text("Update"),
+                      trailing: Icon(Icons.edit),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    child: ListTile(
+                      title: Text("Delete"),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
